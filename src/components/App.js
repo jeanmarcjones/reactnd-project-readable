5c2e9d8c2309882a3d89d9d5f9e8a69b1a4b5b4f
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2017-12-25 12:55:10 +0100

    style: double space between class names

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import CategoriesNav from './CategoriesNav'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <header className="hero is-primary">
+        <header className="hero  is-primary">
           <div className="hero-body">
             <div className="container">
               <h1 className="title">
@@ -13,7 +14,7 @@ class App extends Component {
             </div>
           </div>
         </header>
-
+        <CategoriesNav/>
       </div>
     );
   }
