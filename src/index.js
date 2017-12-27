547a4e7d8b2d60a3172585381662ced8c92e8359
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2017-12-27 11:07:35 +0100

    feat: use provider to pass store to all components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './components/App';
+import reducer from './reducers'
 import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 ReactDOM.render(
-  <App />, document.getElementById('root'));
-registerServiceWorker();
+  <Provider store={store}>
+    <App/>
+  </Provider>, document.getElementById('root'))
+registerServiceWorker()
