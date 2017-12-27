f1b0acdcb269e47d87658fa7a12ba2b3e16dc052
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2017-12-27 11:29:55 +0100

    feat: populate categories tabs from store

--- src/components/CategoriesNav.js
+++ src/components/CategoriesNav.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getCategories } from '../actions'
 
 class CategoriesNav extends Component {
+  componentDidMount() {
+    this.props.getCategories()
+  }
   render() {
     return (
       <div className="tabs  is-centered">
-        <ul>
-          <li><a>Pictures</a></li>
-          <li><a>Music</a></li>
-          <li><a>Videos</a></li>
-          <li><a>Documents</a></li>
+        <ul className="capitalise">
+          {this.props.categories.map((category) => (
+            <li key={category.path}><a>{category.name} </a></li>
+          ))}
         </ul>
       </div>
     );
   }
 }
 
-export default CategoriesNav
+const mapStateToProps = (state) => ({
+  categories: state.categories
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  getCategories: () => dispatch(getCategories())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoriesNav)
