24982b387f800c877180220d486f1cf0414fdc84
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2017-12-28 13:58:11 +0100

    feat: added logger and thunk middleware

--- src/index.js
+++ src/index.js
@@ -3,19 +3,20 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import logger from 'redux-logger'
-import './index.css';
-import App from './components/App';
+import thunk from 'redux-thunk';
+import './index.css'
+import App from './components/App'
 import reducer from './reducers'
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   reducer,
   composeEnhancers(
-    applyMiddleware(logger)
+    applyMiddleware(logger, thunk)
   )
-)
+);
 
 ReactDOM.render(
   <Provider store={store}>
