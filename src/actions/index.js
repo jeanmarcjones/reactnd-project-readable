b1a58cec66c50681913f5d354671a51ca4145868
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2017-12-28 13:46:54 +0100

    feat: created action and middleware to fetch categories

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import * as CategoriesAPI from '../utils/api'
+
+export const RECEIVE_CATEGORY = 'RECEIVE_CATEGORY'
+
+export const receiveCategories = (categories) => ({
+  type: RECEIVE_CATEGORY,
+  categories
+})
+
+export const fetchCategories = () => (dispatch) => (
+  CategoriesAPI
+    .fetchCategories()
+    .then((res) => {
+      dispatch(receiveCategories(res))
+    })
+)
