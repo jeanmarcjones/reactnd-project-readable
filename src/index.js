31189ecdbc32013a635e58ec312947a4873bd712
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2017-12-30 12:51:49 +0000

    feat: Added react router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import logger from 'redux-logger'
-import thunk from 'redux-thunk';
+import thunk from 'redux-thunk'
 import './index.css'
 import App from './components/App'
 import reducer from './reducers'
@@ -20,6 +21,8 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </Provider>, document.getElementById('root'))
 registerServiceWorker()
