2f1083ca63dc847f41cce8b8f22fe3db3aa8134b
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2017-12-30 10:50:26 +0000

    feat: created add category action

--- src/actions/categories.js
+++ src/actions/categories.js
@@ -1,10 +1,17 @@
 import * as CategoriesAPI from '../utils/api_categories'
 
 export const RECEIVE_CATEGORY = 'RECEIVE_CATEGORY'
+export const ADD_CATEGORY = 'ADD_CATEGORY'
 
 export const receiveCategories = (categories) => ({
   type: RECEIVE_CATEGORY,
   categories
+})
+
+export const addCategory = ({ name, path }) => ({
+  type: ADD_CATEGORY,
+  name,
+  path
 })
 
 export const fetchCategories = () => (dispatch) => (
