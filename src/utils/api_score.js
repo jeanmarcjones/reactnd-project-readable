c44f49faa240738b8377bc6ff261246ed1697f47
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-01-07 19:48:01 +0000

    feat: created set score api call for voting

---
+++ src/utils/api_score.js
@@ -0,0 +1,11 @@
+import * as API from './api'
+
+export const setScore = (id, component, option) =>
+  fetch(`${API.url}/${component}/${id}`, {
+    method: "POST",
+    headers: {
+      ...API.headers,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ option })
+  }).then(res => res.json())
