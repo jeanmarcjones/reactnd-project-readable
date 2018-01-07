bdb551675a2e5ed0177bea5d29c94d32768f7afd
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-01-07 19:31:02 +0000

    style: changed quote type

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,6 +6,6 @@ if (!token)
   token = localStorage.token = Math.random().toString(36).substr(-8)
 
 export const headers = {
-  'Accept': 'application/json',
-  'Authorization': token
+  Accept: "application/json",
+  Authorization: token
 }
