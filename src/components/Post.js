a7cfe8662d76e6300403fec7fcb16f4064053008
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-01-07 19:53:53 +0000

    feat: added styling and vote score component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import VoteScore from './VoteScore'
 import GoComment from 'react-icons/lib/go/comment'
 
 function Post(props) {
@@ -6,7 +7,16 @@ function Post(props) {
     <div className="box">
       <p className="title">{props.post.title}</p>
       <p className="subtitle  capitalise">Author: <strong>{props.post.author}</strong></p>
-      <p><GoComment style={{ verticalAlign: 'text-top' }}/> {props.post.commentCount}</p>
+      <div className='post__comment-count'>
+        <GoComment style={{ verticalAlign: 'text-top' }}/> {props.post.commentCount}
+      </div>
+      <VoteScore
+        info={{
+          score: props.post.voteScore,
+          id: props.post.id,
+          component: 'posts'
+        }}
+      />
     </div>
   )
 }
