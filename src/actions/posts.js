31bdb1da97f1b6b591b6b936417fefe4670fef05
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-01-10 20:15:16 +0000

    feat: add a single post to store

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -2,12 +2,31 @@ import * as PostsAPI from '../utils/api_posts'
 import * as ScoreAPI from '../utils/api_score'
 
 export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+export const GET_POST = 'GET_POST'
 export const UPDATE_POST = 'UPDATE_POST'
 
 export const receivePosts = (posts) => ({
   type: RECEIVE_POSTS,
   posts
 })
+
+export const getPost = (post) => ({
+  type: GET_POST,
+  post
+})
+
+export const updatePost = (post) => ({
+  type: UPDATE_POST,
+  post
+})
+
+export const fetchPost = ({ id }) => (dispatch) => (
+  PostsAPI
+    .fetchPost(id)
+    .then((res) => {
+      dispatch(getPost(res))
+    })
+)
 
 export const fetchPosts = () => (dispatch) => (
   PostsAPI
@@ -20,11 +39,6 @@ export const fetchPosts = () => (dispatch) => (
       dispatch(receivePosts(res))
     })
 )
-
-export const updatePost = (post) => ({
-  type: UPDATE_POST,
-  post
-})
 
 export const updateScore = ({ id, component, option }) => (dispatch) => {
   ScoreAPI
