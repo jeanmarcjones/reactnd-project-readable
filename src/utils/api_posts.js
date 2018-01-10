a3774634c29e74ad7c2b2d847655c69e171cf598
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-01-10 20:14:02 +0000

    feat: get a single post from server

--- src/utils/api_posts.js
+++ src/utils/api_posts.js
@@ -1,5 +1,14 @@
 import * as API from './api'
 
+export const fetchPost = (id) =>
+  fetch(`${API.url}/posts/${id}`, {
+    method: "GET",
+    headers: {
+      ...API.headers,
+      "Content-Type": "application/json"
+    }
+  }).then(res => res.json())
+
 export const fetchPosts = () =>
   fetch(`${API.url}/posts`, { headers: API.headers })
     .then(res => res.json())
