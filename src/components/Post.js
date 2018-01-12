14bc9f9ca3a41332ad2ae20fddc0165a3d596dcd
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-01-12 14:39:18 +0000

    refactor: added post actions buttons

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,27 +1,52 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import VoteScore from './VoteScore'
 import CommentCount from './CommentCount'
+import { removePost } from '../actions/posts'
+
 
 function Post(props) {
   return (
     <div className="box">
       <p className="title">{props.post.title}</p>
       <p className="subtitle  capitalise">Author: <strong>{props.post.author}</strong></p>
-      <CommentCount count={props.post.commentCount}/>
-      <VoteScore
-        info={{
-          score: props.post.voteScore,
-          id: props.post.id,
-          component: 'posts'
-        }}
-      />
-      <Link
-        to={`/${props.post.category}/${props.post.id}`}
-        style={{marginLeft: '20px'}}
-      >Info</Link>
+      <div className="level">
+        <div className="level-left">
+          <CommentCount
+            count={props.post.commentCount}
+            className="level-item"
+          />
+          <VoteScore
+            info={{
+              score: props.post.voteScore,
+              id: props.post.id,
+              component: 'posts'
+            }}
+            className="level-item"
+          />
+        </div>
+        <div className="level-right">
+          <Link
+            to={`/${props.post.category}/${props.post.id}`}
+            className="level-item  button"
+          >Details</Link>
+          <p className="level-item  button">Edit</p>
+          <a
+            onClick={() => props.delete({ id: props.post.id })}
+            className="level-item  button  is-danger  is-outlined"
+          >Delete</a>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Post
+const mapDispatchToProps = (dispatch) => ({
+  delete: (data) => dispatch(removePost(data))
+})
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Post)
