8679bf0b60f6c4e5bf3f257aa2f4d228a150bf86
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-01-13 15:08:57 +0000

    feat: created add post api call

--- src/utils/api_posts.js
+++ src/utils/api_posts.js
@@ -13,6 +13,16 @@ export const fetchPosts = () =>
   fetch(`${API.url}/posts`, { headers: API.headers })
     .then(res => res.json())
 
+export const createPost = (post) =>
+  fetch(`${API.url}/posts`, {
+    method: "POST",
+    headers: {
+      ...API.headers,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(post)
+  }).then(res => res.json())
+
 export const deletePost = ({ id }) =>
   fetch(`${API.url}/posts/${id}`, {
     method: "DELETE",
