5683d0894a100d103bfec2081943c700f24e7a98
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-01-15 12:11:12 +0000

    refactor: added comments reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import categories from './categories'
 import posts from './posts'
+import comments from './comments'
 import { reducer as formReducer } from 'redux-form'
 
 export default combineReducers({
   categories,
   posts,
+  comments,
   form: formReducer
 })
