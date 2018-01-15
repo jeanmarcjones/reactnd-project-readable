698d9df1f31a15c1c811d8b8d08e763ca92ff099
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-01-15 15:39:45 +0000

    feat: created comment list component

---
+++ src/components/CommentsLists.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import Comment from './Comment'
+
+function CommentsList({ comments }) {
+  return comments.length > 0
+    ? comments.map((comment) => (
+      <Comment key={comment.id}  comment={comment}/>
+    ))
+    : <article className="media">
+      <div className="media-content">
+        <div className="content">
+          <p>This posts has no comments.</p>
+        </div>
+      </div>
+    </article>
+}
+
+const mapStateToProps = ({ comments }, ownProps) => ({
+  comments: comments.allIds
+    .filter((id) => comments.byId[id].parentId === ownProps.postId)
+    .map((id) => comments.byId[id])
+})
+
+export default connect(
+  mapStateToProps
+)(CommentsList)
