69620016483a4da7e11677e70d9d8c79a19ec840
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-01-15 16:04:20 +0000

    feat: you can now delete comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,31 +1,33 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { deleteComment } from '../actions/comments'
 import VoteScore from './VoteScore'
 
-function Comment({ comment }) {
+function Comment(props) {
   return (
     <article className="media">
       <div className="media-content">
         <div  className="content  comment">
-          <h4 className="title">{comment.body}</h4>
-          <h5 className="subtitle">By <strong>{comment.author}</strong></h5>
+          <h4 className="title">{props.comment.body}</h4>
+          <h5 className="subtitle">By <strong>{props.comment.author}</strong></h5>
           <div className="level">
             <div className="level-left">
               <VoteScore
                 info={{
-                  score: comment.voteScore,
-                  id: comment.id,
+                  score: props.comment.voteScore,
+                  id: props.comment.id,
                   component: 'posts'
                 }}
               />
             </div>
             <div className="level-right">
               <Link
-                to={`/edit-comment/${comment.id}`}
+                to={`/edit-comment/${props.comment.id}`}
                 className="level-item  button"
               >Edit</Link>
               <a
-                // onClick={() => props.delete({ id: comment.id })}
+                onClick={() => props.delete({ id: props.comment.id })}
                 className="level-item  button  is-danger  is-outlined"
               >Delete</a>
             </div>
@@ -36,4 +38,11 @@ function Comment({ comment }) {
   )
 }
 
-export default Comment
+const mapDispatchToProps = (dispatch) => ({
+  delete: (data) => dispatch(deleteComment(data))
+})
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Comment)
