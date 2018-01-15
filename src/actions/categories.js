872aaf770a2b04176cc00bc94cb0203bbc3b641c
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-01-15 12:22:31 +0000

    refactor: action takes object as parameter

--- src/actions/categories.js
+++ src/actions/categories.js
@@ -3,7 +3,7 @@ import * as CategoriesAPI from '../utils/api_categories'
 export const RECEIVE_CATEGORY = 'RECEIVE_CATEGORY'
 export const ADD_CATEGORY = 'ADD_CATEGORY'
 
-export const receiveCategories = (categories) => ({
+export const receiveCategories = ({ categories }) => ({
   type: RECEIVE_CATEGORY,
   categories
 })
@@ -22,6 +22,6 @@ export const fetchCategories = () => (dispatch) => (
       [category.name]: category
     }))))
     .then((res) => {
-      dispatch(receiveCategories(res))
+      dispatch(receiveCategories({ categories: res }))
     })
 )
