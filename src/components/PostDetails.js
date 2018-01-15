c7cb9ce67aaae0f8b56f39f381f0a3ac87423c4f
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-01-15 15:40:11 +0000

    refactor: added comment list and buttons

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import CommentsList from './CommentsLists'
 import CommentCount from './CommentCount'
 import VoteScore from './VoteScore'
 
 class PostDetails extends Component {
   render() {
-    const { post } = this.props
+    const { post, match } = this.props
 
     // Waits till post has been populated before showing details
     return this.props.post
       ? <div className="container">
         <article className="media">
           <div className="media-content">
-            <div className="content">
+            <div className="content  post-details">
               <p className="title  is-3"><strong>{post.title}</strong></p>
               <p className="subtitle  is-5">By <strong>{post.author}</strong></p>
               <CommentCount count={post.commentCount}/>
@@ -23,21 +25,38 @@ class PostDetails extends Component {
                   component: 'posts'
                 }}
               />
-              <p style={{marginTop: 15}}>
+              <p className="post-details__body">
                 {post.body}
-                <br/>
-                <small><a>Like</a> · <a>Reply</a> · 3 hrs</small>
               </p>
-            </div>
+              <div className="post-details__buttons">
+                <Link
+                  to={`/edit-comment/${post.id}`}
+                  className="button"
+                >Edit</Link>
+                <a
+                  // onClick={() => props.delete({ id: post.id })}
+                  className="button  is-danger  is-outlined"
+                >Delete</a>
+                <Link
+                  to='/add-comment'
+                  className="button  is-success  is-outlined"
+                >Add Comment</Link>
+              </div>
           </div>
+          <CommentsList postId={match.params.id}/>
+        </div>
         </article>
       </div>
       : <p>Loading</p>
   }
 }
 
-const mapStateToProps = ({ posts }, ownProps) => ({
-  post: posts.byId[ownProps.match.params.id],
-})
+const mapStateToProps = ({ posts }, ownProps) => {
+  const postId = ownProps.match.params.id
+
+  return {
+    post: posts.byId[postId]
+  }
+}
 
 export default connect(mapStateToProps)(PostDetails)
