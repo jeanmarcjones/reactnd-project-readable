ee1fa3ef20ad755f48f30b369fbbfa06e4332847
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-01-15 12:16:17 +0000

    feat: created get posts comments api call

--- src/utils/api_posts.js
+++ src/utils/api_posts.js
@@ -13,6 +13,15 @@ export const fetchPosts = () =>
   fetch(`${API.url}/posts`, { headers: API.headers })
     .then(res => res.json())
 
+export const fetchPostComments = (id) =>
+  fetch(`${API.url}/posts/${id}/comments`, {
+    method: "GET",
+    headers: {
+      ...API.headers,
+      "Content-Type": "application/json"
+    }
+  }).then(res => res.json())
+
 export const createPost = (post) =>
   fetch(`${API.url}/posts`, {
     method: "POST",
