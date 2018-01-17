e60c0396c9a654768c368be15505b170dba23d51
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-01-17 20:04:09 +0000

    refactor: can add new comment with modal
    Also turned into function stateless component

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -1,63 +1,76 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { removePost } from "../actions/posts";
+import Modal from 'react-modal'
+import { removePost } from "../actions/posts"
+import  { addCommentModalOpen, addCommentModalClose } from '../actions/modal'
 import CommentsList from './CommentsLists'
 import CommentCount from './CommentCount'
 import VoteScore from './VoteScore'
+import AddComment from './AddComment'
 
-class PostDetails extends Component {
-  render() {
-    const { post, match } = this.props
-
-    // Waits till post has been populated before showing details
-    return this.props.post
-      ? <div className="container">
-        <article className="media">
-          <div className="media-content">
-            <div className="content  post-details">
-              <p className="title  is-3"><strong>{post.title}</strong></p>
-              <p className="subtitle  is-5">By <strong>{post.author}</strong></p>
-              <CommentCount count={post.commentCount}/>
-              <VoteScore
-                info={{
-                  score: post.voteScore,
-                  id: post.id,
-                  component: 'posts'
-                }}
-              />
-              <p className="post-details__body">
-                {post.body}
-              </p>
-              <div className="post-details__buttons">
-                <Link
-                  to={`/edit-comment/${post.id}`}
-                  className="button"
-                >Edit</Link>
-                <a
-                  onClick={() => this.props.delete({ id: post.id })}
-                  className="button  is-danger  is-outlined"
-                >Delete</a>
-                <Link
-                  to='/add-comment'
-                  className="button  is-success  is-outlined"
-                >Add Comment</Link>
-              </div>
-          </div>
-          <CommentsList postId={match.params.id}/>
+function PostDetails({ post, match, remove, addCommentModalOpen, openAddCommentModal, closeAddCommentModal }) {
+  // Waits till post has been populated before showing details
+  return post
+    ? <div className="container">
+      <article className="media">
+        <div className="media-content">
+          <div className="content  post-details">
+            <p className="title  is-3"><strong>{post.title}</strong></p>
+            <p className="subtitle  is-5">By <strong>{post.author}</strong></p>
+            <CommentCount count={post.commentCount}/>
+            <VoteScore
+              info={{
+                score: post.voteScore,
+                id: post.id,
+                component: 'posts'
+              }}
+            />
+            <p className="post-details__body">
+              {post.body}
+            </p>
+            <div className="post-details__buttons">
+              <a
+                // onClick={() => this.openIngredientsModal}
+                className="button"
+              >Edit</a>
+              <a
+                onClick={() => remove({ id: post.id })}
+                className="button  is-danger  is-outlined"
+              >Delete</a>
+              <div
+                onClick={() => openAddCommentModal()}
+                className="button  is-success  is-outlined"
+              >Add Comment</div>
+            </div>
         </div>
-        </article>
+        <CommentsList postId={match.params.id}/>
       </div>
-      : <p>Loading</p>
-  }
+      </article>
+      {addCommentModalOpen && (
+        <Modal
+          isOpen={addCommentModalOpen}
+          onRequestClose={closeAddCommentModal}
+          contentLabel='Add Comment'
+          appElement={document.getElementById('root')}
+        >
+          {addCommentModalOpen && (
+            <AddComment id={post.id}/>
+          )}
+        </Modal>
+      )}
+    </div>
+    : <p>Loading</p>
 }
 
-const mapStateToProps = ({ posts }, ownProps) => ({
-  post: posts.byId[ownProps.match.params.id]
+const mapStateToProps = ({ posts, modal }, ownProps) => ({
+  post: posts.byId[ownProps.match.params.id],
+  addCommentModalOpen: modal.addCommentModalOpen
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  delete: (data) => dispatch(removePost(data))
+  remove: (data) => dispatch(removePost(data)),
+  openAddCommentModal: (data) => dispatch(addCommentModalOpen(data)),
+  closeAddCommentModal: (data) => dispatch(addCommentModalClose(data))
 })
 
 export default connect(
