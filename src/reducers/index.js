c69cd59661c74f81cdbb89811eb00b7de101b9b8
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-01-17 19:57:13 +0000

    refactor: added modal reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import categories from './categories'
 import posts from './posts'
 import comments from './comments'
+import modal from './modal'
 import { reducer as formReducer } from 'redux-form'
 
 export default combineReducers({
   categories,
   posts,
   comments,
+  modal,
   form: formReducer
 })
