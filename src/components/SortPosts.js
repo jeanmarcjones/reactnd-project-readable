76e4af205db020f927b50e36f409412f8e9d9e1e
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-01-20 16:58:03 +0000

    refactor: tags now sort posts on click

--- src/components/SortPosts.js
+++ src/components/SortPosts.js
@@ -1,17 +1,32 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { sortPost } from '../actions/posts'
 
-function SortPosts({}) {
+function SortPosts({ sort }) {
   return(
     <div>
       <p className="tags--title  is-size-4  has-text-weight-semibold">
         Sort Posts:
       </p>
       <div className="tags">
-        <span className="tag  is-medium">Score</span>
-        <span className="tag  is-medium">Date</span>
+        <span
+          onClick={() => sort({ sortKey: 'voteScore' })}
+          className="tag  is-medium"
+        >Score</span>
+        <span
+          onClick={() => sort({ sortKey: 'timestamp' })}
+          className="tag  is-medium"
+        >Date</span>
       </div>
     </div>
   )
 }
 
-export default SortPosts
+const mapDispatchToProps = (dispatch) => ({
+  sort: (data) => dispatch(sortPost(data))
+})
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(SortPosts)
